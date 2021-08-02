bd30aedae2227a420a67f4ae7d99d2ba30a0c5b0
Tharuka Bandara <33684419+tharukab95@users.noreply.github.com>
2021-08-02 07:43:08 +0530

    add main-layout lib

---
+++ sample/libs/tuition/ui/main-layout/README.md
@@ -0,0 +1,7 @@
+# tuition-ui-main-layout
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test tuition-ui-main-layout` to execute the unit tests.
---
+++ sample/libs/tuition/ui/main-layout/src/lib/tuition-ui-main-layout.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Route } from '@angular/router';
+
+export const tuitionUiMainLayoutRoutes: Route[] = [];
+
+@NgModule({
+  imports: [CommonModule, RouterModule],
+})
+export class TuitionUiMainLayoutModule {}
