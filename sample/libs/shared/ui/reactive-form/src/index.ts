87aaa348152943414baa2a16aa9337f2203f412e
Tharuka Bandara <33684419+tharukab95@users.noreply.github.com>
2021-08-13 20:16:47 +0530

    deleted shared auth library in frontend

--- sample/libs/shared/ui/reactive-form/src/index.ts
+++ sample/libs/shared/ui/reactive-form/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/shared-ui-reactive-form.module';
+export * from './lib/+state/ngrx-forms.actions';
+export * from './lib/+state/ngrx-forms.reducer';
+export * from './lib/+state/ngrx-forms.facade';
+export * from './lib/+state/ngrx-forms.interfaces';
--- sample/libs/shared/ui/reactive-form/src/lib/shared-ui-reactive-form.module.ts
+++ sample/libs/shared/ui/reactive-form/src/lib/shared-ui-reactive-form.module.ts
@@ -2,9 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { NgrxFormsEffects } from './+state/ngrx-forms.effects';
+import { NgrxFormsFacade } from './+state/ngrx-forms.facade';
+import { ngrxFormsInitialState, ngrxFormsReducer, ngrxFormsFeatureKey } from './+state/ngrx-forms.reducer';
+import { DynamicFieldDirective } from './dynamic-form/dynamic-field.directive';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { InputComponent } from './fields/input/input.component';
+import { TextareaComponent } from './fields/textarea/textarea.component';
+import { ListErrorsComponent } from './list-errors/list-errors.component';
+
+
 export const sharedUiReactiveFormRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    StoreModule.forFeature(ngrxFormsFeatureKey, ngrxFormsReducer, {
+     initialState: ngrxFormsInitialState,
+    }),
+    ReactiveFormsModule,
+    StoreModule.forFeature(ngrxFormsFeatureKey, ngrxFormsReducer, {
+      initialState: ngrxFormsInitialState,
+    }),
+    EffectsModule.forFeature([NgrxFormsEffects]),
+  ]
 })
 export class SharedUiReactiveFormModule {}
